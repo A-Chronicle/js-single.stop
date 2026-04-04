2e83ed3e67fef80f87b9303290f0ba77832f0c30
Anshika chaubey <155385356+A-Chronicle@users.noreply.github.com>
2026-04-04 17:13:20 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,2 +1,14 @@
 "use strict" // treat all js code as newer version
-// alert 
+
+
+// number =
+// bigint =
+// string =""
+// boolean = true/false
+// null = standalone value
+// undefined = value is not assigned yet 
+// symbol = unique
+
+// object 
+
+console.log(typeof "The Travelling Monk");
