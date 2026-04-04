908ab5a16c4bcd3f035bce99f96a52e9fd4cf5f7
Anshika chaubey <155385356+A-Chronicle@users.noreply.github.com>
2026-04-04 15:45:59 +0000

    new file added

---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "anshika08@gmail.com"
+var accountPassword = "12345"
+accountCity = "Varanasi"
+
+console.table([accountId ,accountEmail,accountPassword,accountCity]);
